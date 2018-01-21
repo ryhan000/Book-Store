eafab9de80518eef38a2a48b8f0613b86d5ee01a
ryhan000 <rayhan1ahamed@gmail.com>
2018-01-21 19:16:15 +0600

    Added  More to Login

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.bookstore.config;
 
-import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.bookstore.service.Impl.UserSecurityService;
+import com.bookstore.service.Utility.SecurityUtility;
 
 @Configuration
 @EnableWebSecurity
